eb2fb977d40122955f58d4ff9df15527b8fbaa71
jtejeda501 <julio.tejeda.ingeniero@gmail.com>
2025-06-20 15:09:38 -0600

    deploy server

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ ssh -o StrictHostKeyChecking=no azureuser@20.55.28.37 <<HTML
 
     cd /home/azureuser/git-actions-course.github.io
 
-    git pull --rebase origin main
-    
+    git pull --rebase origin master
+
 HTML
